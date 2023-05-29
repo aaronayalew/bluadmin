152d209bf3e44e898ff5f8369f20122e5e34c28d
Aaron Ayalew <30746329+aaronayalew@users.noreply.github.com>
2023-05-29 16:24:49 +0300

    Update sms_module.php

--- app/CentralLogics/sms_module.php
+++ app/CentralLogics/sms_module.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Nexmo\Laravel\Facade\Nexmo;
 use Twilio\Rest\Client;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 
 class SMS_module
@@ -40,7 +40,9 @@ class SMS_module
         //     $response = self::msg_91($receiver, $otp);
         //     return $response;
         // }
-        $response = Http::get("https://api.afromessage.com/api/send?from=e80ad9d8-adf3-463f-80f4-7c4b39f7f164&to=$receiver&message=Your otp is $otp");
+        $response = Http::withHeaders([
+    'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZGVudGlmaWVyIjoialkzTk5wWnYxOVpMblNwTTNQUjdDbTJyUGZWcHpSa2kiLCJleHAiOjE4NDMyMTk0NjgsImlhdCI6MTY4NTM2NjY2OCwianRpIjoiMmMyZTE1ODgtOWFjOS00NTM2LTg1OTUtZmYyNzllMzE0N2FhIn0.96Qz4pZXNvUwrAWQrJI6RF0AaO2j6KHcHfpP6EERZhk'
+])get("https://api.afromessage.com/api/send?from=e80ad9d8-adf3-463f-80f4-7c4b39f7f164&to=$receiver&message=Your otp is $otp");
         Log::error($response->status()->toString());
         return "success";
     }
